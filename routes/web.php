f3f0e740cfcb5f551e5db3d06b09a05c0c3304a7
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2025-04-05 13:10:57 +0300

    add editing company feature

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Livewire\CompanyCreate;
+use App\Livewire\CompanyEdit;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/company-create', \App\Livewire\CompanyCreate::class);
+Route::get('companies/create', CompanyCreate::class)
+    ->name('companies.create');
+Route::get('companies/{company}/edit', CompanyEdit::class)
+    ->name('companies.edit');
