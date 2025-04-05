4d789159eb457e50ce0d0d7bd15fa145b78484de
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2025-04-05 13:13:24 +0300

    use Form for editing

--- app/Livewire/CompanyEdit.php
+++ app/Livewire/CompanyEdit.php
@@ -2,36 +2,28 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Forms\CompanyForm;
 use App\Models\City;
 use App\Models\Company;
 use App\Models\Country;
 use Illuminate\Support\Collection;
-use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class CompanyEdit extends Component
 {
+    public CompanyForm $form;
     public Collection $countries;
     public Collection $cities;
     public Company $company;
-
-    #[Validate('required|min:3', onUpdate: false)]
-    public string $name = '';
-
-    #[Validate('required', onUpdate: false)]
-    public string $country = '';
-
-    #[Validate('required', onUpdate: false)]
-    public string $city = '';
 
     public string $savedName = '';
 
     public function mount(Company $company)
     {
-        $this->name = $company->name;
-        $this->country = $company->country_id;
+        $this->form->name = $company->name;
+        $this->form->country = $company->country_id;
         $this->cities = City::where('country_id', $this->country)->get();
-        $this->city = $company->city_id;
+        $this->form->city = $company->city_id;
 
         $this->countries = Country::all();
     }
@@ -39,8 +31,8 @@ class CompanyEdit extends Component
     public function updated($property)
     {
         if ($property == 'country') {
-            $this->cities = City::where('country_id', $this->country)->get();
-            $this->city = $this->cities->first()->id;
+            $this->cities = City::where('country_id', $this->form->country)->get();
+            $this->form->city = $this->cities->first()->id;
         }
     }
 
@@ -54,11 +46,11 @@ class CompanyEdit extends Component
         $this->validate();
 
         $this->company->update([
-            'name' => $this->name,
-            'country_id' => $this->country,
-            'city_id' => $this->city,
+            'name' => $this->form->name,
+            'country_id' => $this->form->country,
+            'city_id' => $this->form->city
         ]);
 
-        $this->savedName = $this->name;
+        $this->savedName = $this->form->name;
     }
 }
